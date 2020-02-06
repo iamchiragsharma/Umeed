c9939af9e6f03ca739136769ff25b0fe1dce1e7f
iamchiragsharma <chiragsharmagtbit@outlook.com>
2020-02-06 22:21:03 +0530

    "hashtag monitoring added - tested"

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 - [x] Development of api's for fetching profile,adding and removing them from suspect list
 - [x] Development of WebApp and real time front page state wise monitoring
 - [x] Child Predator Identification from Chat
-- [x] Development of Profile Analysis 
+- [x] Development of Profile Analysis
+- [ ] Creation of Suspect List and it's Monitoring
+- [ ] Addition of Interactive Graphs for Profile Analysis 
 - [ ] Development of Hashtag Analysis
 - [ ] Development of Voilence Detection
 - [ ] Development of Nudity Detection
--- base.py
+++ base.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask,render_template,url_for,request
 from testing_var import test
 
@@ -6,6 +7,7 @@ from sentiment import get_profile,profile_hashtag_analyzer
 
 app = Flask(__name__,static_url_path='/static')
 
+suspect = {}
 
 @app.route('/')
 def index():
@@ -15,11 +17,40 @@ def index():
 
 @app.route('/profile',methods=["GET","POST"])
 def profile():
+    global suspect
     if request.method == "POST":
         twitter_id = request.form.get("tweetbox")
-        user_profile = get_profile(twitter_id)
-        user_data = profile_hashtag_analyzer(twitter_id)
-        return render_template("profile.html",user_profile=user_profile,user_data=user_data)
+        if twitter_id.startswith("#"):
+            hashtag_data = profile_hashtag_analyzer(twitter_id)
+            return render_template("hashtag.html",hashtag_data=hashtag_data)
+        else:
+            user_profile = get_profile(twitter_id)
+            user_data = profile_hashtag_analyzer(twitter_id)
+            suspect = user_profile
+            return render_template("profile.html",user_profile=user_profile,user_data=user_data)
+
+@app.route('/suspectprofiles',methods=["GET","POST"])
+def suspectprofiles():
+    global suspect
+    if request.method == "POST":
+        with open("datalogs/suspect.csv","a+") as csvFile:
+            writer = csv.writer(csvFile)
+            row = [suspect['name'],suspect['username'],suspect['profile_photo'],suspect['likes_count'], suspect['tweets_count'], suspect['followers_count'], suspect['following_count']]
+            writer.writerow(row)
+        
+        with open("datalogs/suspect.csv","r") as csvFile:
+            reader = csv.reader(csvFile)
+            suspects = [row for row in reader]
+        
+    suspects = reversed(suspects)
+    return render_template("suspectprofiles.html",suspects=suspects)
+
+@app.route("/hashtag_profile/<twitter_id>")
+def hashtag_profile(twitter_id):
+    user_profile = get_profile(twitter_id)
+    user_data = profile_hashtag_analyzer(twitter_id)
+    suspect = user_profile
+    return render_template("profile.html",user_profile=user_profile,user_data=user_data)
 
 
 if __name__ == '__main__':
@@ -36,3 +67,9 @@ if __name__ == '__main__':
 
 # {% endfor %}
 # {% endfor %}
+
+# <div class="btn-group" role="group" aria-label="Basic example">
+# <button type="button" class="btn btn-danger">Sexual : 2 </button>
+# <button type="button" class="btn btn-danger">Hate Speech/Voilence : 2</button>
+# <button type="button" class="btn btn-danger">Child Predator : No </button>
+# </div>
